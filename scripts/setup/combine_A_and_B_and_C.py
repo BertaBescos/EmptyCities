b31b93a0c89ceb4e03e5c65aae0672063ab6c6f5
BertaBescos <berta_zgz93@hotmail.com>
2018-08-23 12:54:59 +0200

    setup

---
+++ scripts/setup/combine_A_and_B_and_C.py
@@ -0,0 +1,49 @@
+from pdb import set_trace as st
+import os
+import numpy as np
+import cv2
+import argparse
+import skimage.io
+
+parser = argparse.ArgumentParser('create image setup')
+parser.add_argument('--fold_A', dest='fold_A', help='input directory for image A', type=str)
+parser.add_argument('--fold_B', dest='fold_B', help='input directory for image B', type=str)
+parser.add_argument('--fold_C', dest='fold_C', help='input directory for image C', type=str)
+parser.add_argument('--fold_ABC', dest='fold_ABC', help='output directory', type=str)
+
+for arg in vars(args):
+	print('[%s] = ' % arg,  getattr(args, arg))
+
+splits = filter( lambda f: not f.startswith('.'), os.listdir(args.fold_A)) # ignore hidden folders like .DS_Store
+
+for sp in splits:
+	img_fold_A = os.path.join(args.fold_A, sp)
+	img_fold_B = os.path.join(args.fold_B, sp)
+	img_fold_C = os.path.join(args.fold_C, sp)
+	img_list = filter( lambda f: not f.startswith('.'), os.listdir(img_fold_A)) # ignore hidden folders like .DS_Store
+	num_imgs = len(img_list)
+	print('split = %s, use %d/%d images' % (sp, num_imgs, len(img_list)))
+	img_fold_ABC = os.path.join(args.fold_ABC, sp)
+	if not os.path.isdir(img_fold_ABC):
+		os.makedirs(img_fold_ABC)
+	print('split = %s, number of images = %d' % (sp, num_imgs))
+	for n in range(num_imgs):
+		name_A = img_list[n]
+		path_A = os.path.join(img_fold_A, name_A)
+		name_B = name_A
+		name_C = name_A
+		path_B = os.path.join(img_fold_B, name_B)
+		path_C = os.path.join(img_fold_C, name_C)
+		if os.path.isfile(path_A) and os.path.isfile(path_B) and os.path.isfile(path_C):
+			name_ABC = name_A
+			path_ABC = os.path.join(img_fold_ABC, name_ABC)
+			im_A = cv2.imread(path_A, cv2.IMREAD_COLOR)
+			im_B = cv2.imread(path_B, cv2.IMREAD_COLOR)
+			im_C = skimage.io.imread(path_C)
+			_im_C = im_C.copy()
+			_im_C[:,:,:] = 0
+			_im_C[im_C[:,:,0] == 4] = 255		
+			_im_C[im_C[:,:,0] == 10] = 255
+			im_C = _im_C.copy()
+			im_ABC = np.concatenate([im_A, im_B, im_C], 1)
+			cv2.imwrite(path_ABC, im_ABC)
---
+++ scripts/setup/setup.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+src="$1"
+dest="$2"
+if [ ! -d "$dest" ]; then
+	mkdir $dest
+fi
+dest_A=$dest"A/"
+if [ ! -d "$dest_A" ]; then
+	mkdir $dest_A
+fi
+dest_B=$dest"B/"
+if [ ! -d "$dest_B" ]; then
+	mkdir $dest_B
+fi
+dest_C=$dest"C/"
+if [ ! -d "$dest_C" ]; then
+	mkdir $dest_C
+fi
+
+train_folders=($src"Town01Train/*/")
+echo $train_folders
+imOut_ini=0
+for folder in "${train_folders[@]}"; do
+	ims=($folder"Dynamic/RGB"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_A"train/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/RGB_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_A"train/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Static/RGB"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_B"train/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Static/RGB_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_B"train/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/SemanticSegmentation"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_C"train/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/SemanticSegmentation_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_C"train/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	imOut_ini=$imOut
+done
+
+test_folders=($src"Town02Test/*/")
+echo $test_folders
+for folder in "${test_folders[@]}"; do
+	ims=($folder"Dynamic/RGB"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_A"test/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/RGB_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_A"test/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Static/RGB"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_B"test/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Static/RGB_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_B"test/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/SemanticSegmentation"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_C"test/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/SemanticSegmentation_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_C"test/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	imOut_ini=$imOut
+done
+
+val_folders=($src"Town01Val/*/")
+echo $val_folders
+for folder in "${val_folders[@]}"; do
+	ims=($folder"Dynamic/RGB"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_A"val/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/RGB_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_A"val/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Static/RGB"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_B"val/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Static/RGB_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_B"val/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/SemanticSegmentation"/*)
+	imOut=$imOut_ini
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_C"val/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	ims=($folder"Dynamic/SemanticSegmentation_back"/*)
+	for im in "${ims[@]}"; do
+		_imOut=`printf %06d $imOut`
+		cp $im $dest_C"val/"$_imOut".png"
+		imOut=$(($imOut + 1))
+	done
+	imOut_ini=$imOut
+done
+
+dest_ABC=$dest"ABC/"
+if [ ! -d "$dest_ABC" ]; then
+	mkdir $dest_ABC
+fi
+
+python combine_A_and_B_and_C.py --fold_A $dest_A --fold_B $dest_B --fold_C $dest_C --fold_ABC $dest_ABC
